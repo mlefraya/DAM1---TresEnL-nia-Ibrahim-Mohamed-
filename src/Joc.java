548dc42f451b80563bfa733ad4361a1a5b9df860
dam1 <mlefraya@elpuig.xeill.net>
2024-04-24 19:57:23 +0200

    Classe Joc

---
+++ src/Joc.java
@@ -0,0 +1,19 @@
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.Scanner;
+
+public class Joc {
+    Scanner sc = new Scanner(System.in);
+    public void NovaPartida() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
+    }
+    public void jugar(int fila, int columna) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
+
+    }
+    public void jugadoraGuanyadora(int fila,int columna) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
+
+    }
+}
+
